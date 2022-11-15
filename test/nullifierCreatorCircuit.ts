f27fec05f0b198987cc20328a2289f789dfa1538
sarmat <skhachirov12@gmail.com>
2022-11-15 16:11:41 -0300

    add negative test case

--- test/nullifierCreatorCircuit.ts
+++ test/nullifierCreatorCircuit.ts
@@ -1,4 +1,4 @@
-import { assert } from 'chai'
+import { expect } from 'chai'
 import { wasm as wasmTester } from 'circom_tester'
 import getNullifier from '../utils/inputs/getNullifier'
 import getNullifierCreatorInputs from '../utils/inputs/getNullifierCreatorInputs'
@@ -13,6 +13,14 @@ describe('NullifierCreator circuit', function () {
     const inputs = await getNullifier(this.baseInputs)
     const witness = await this.circuit.calculateWitness(this.baseInputs)
     await this.circuit.assertOut(witness, {})
-    assert.equal(witness[2], inputs)
+    expect(witness[2]).to.eq(inputs)
+  })
+  it('should fail because inputs are different', async function () {
+    let inputs = await getNullifier(this.baseInputs)
+    const witness = await this.circuit.calculateWitness(this.baseInputs)
+    // corrupt input
+    inputs = BigInt(123)
+    await this.circuit.assertOut(witness, {})
+    expect(witness[2]).to.not.eq(inputs)
   })
 })
