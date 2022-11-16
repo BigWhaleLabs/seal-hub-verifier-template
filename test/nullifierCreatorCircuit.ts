2a8c8640c53a08b632d17dda834312dab4c15d52
Michel Erokhov <mixaileroxov@gmail.com>
2022-11-17 00:10:40 +0300

    fix merkleRoot

--- test/nullifierCreatorCircuit.ts
+++ test/nullifierCreatorCircuit.ts
@@ -1,18 +1,26 @@
+import { BigNumber, utils } from 'ethers'
 import { expect } from 'chai'
 import { wasm as wasmTester } from 'circom_tester'
 import getNullifier from '../utils/inputs/getNullifier'
-import getNullifierCreatorInputs from '../utils/inputs/getNullifierCreatorInputs'
+import getNullifierCreatorInputs, {
+  generateTreeProof,
+} from '../utils/inputs/getNullifierCreatorInputs'
 
 describe('NullifierCreator circuit', function () {
   before(async function () {
     this.circuit = await wasmTester('circuits/NullifierCreator.circom')
-    this.baseInputs = await getNullifierCreatorInputs()
+    this.commitments = Array(99)
+      .fill(undefined)
+      .map(() => BigNumber.from(utils.randomBytes(32)).toBigInt())
+    this.baseInputs = await getNullifierCreatorInputs(this.commitments)
+    this.treeProof = await generateTreeProof(this.commitments, this.baseInputs)
   })
 
   it('should generate the witness successfully', async function () {
     const inputs = await getNullifier(this.baseInputs)
     const witness = await this.circuit.calculateWitness(this.baseInputs)
     await this.circuit.assertOut(witness, {})
+    expect(witness[1]).to.eq(this.treeProof.root)
     expect(witness[2]).to.eq(inputs)
   })
   it('should fail because inputs are different', async function () {
@@ -20,7 +28,13 @@ describe('NullifierCreator circuit', function () {
     const witness = await this.circuit.calculateWitness(this.baseInputs)
     // corrupt input
     inputs = BigInt(123)
+    // corrupt treeProof
+    const commitments = Array(99)
+      .fill(undefined)
+      .map(() => BigNumber.from(utils.randomBytes(32)).toBigInt())
+    const treeProof = await generateTreeProof(commitments, this.baseInputs)
     await this.circuit.assertOut(witness, {})
+    expect(witness[1]).to.not.eq(treeProof.root)
     expect(witness[2]).to.not.eq(inputs)
   })
 })
